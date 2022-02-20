52dc0d494d80a518076f20e9e502e3c031b64afe
mg699 <mg699@kent.ac.uk>
2022-02-20 17:27:57 +0000

    Finished all iterations of part B (all centered around a single function)

--- partB.hs
+++ partB.hs
@@ -1,7 +1,10 @@
 
 import Base
 
--- iteration 1
+-- iteration 1, 2 and 3
+
+slice :: Int -> Int -> String -> String
+slice i j word = take (j-i) (drop i word)
 
 checkGuess :: String -> String -> [(Char, Status)]
 checkGuess guess word = checkGuess' guess word 0
@@ -9,8 +12,9 @@ checkGuess guess word = checkGuess' guess word 0
 checkGuess' :: String -> String -> Int -> [(Char, Status)]
 checkGuess' [] word _ = []
 checkGuess' (x:xs) word cnt
-    | ( x == (word!!cnt) ) = (x, Here) : checkGuess' xs word (cnt+1)
-    | otherwise = (x, Nowhere) : checkGuess' xs word (cnt+1)
-
--- iteration 2
+    | ( x == (word!!cnt) ) = nextGuess Here
+    | ( x `elem` (slice 0 cnt word) ) = nextGuess (Elsewhere (Just Before))
+    | ( x `elem` (slice cnt (length word) word) ) = nextGuess (Elsewhere (Just After))
+    | otherwise = nextGuess Nowhere
+    where nextGuess status = (x, status) : checkGuess' xs word (cnt+1)
 
