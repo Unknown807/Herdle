4a5c61c308bceb648bf210c72bc5401dd4e1d3f1
mg699 <mg699@kent.ac.uk>
2022-02-20 21:47:22 +0000

    Finished iteration 3 of part C

--- partC.hs
+++ partC.hs
@@ -5,17 +5,21 @@ import Data.Char
 -- iteration 1
 
 getGuess :: Int -> String -> IO String
-getGuess n availableChars = getGuess' n "" availableChars
+getGuess n availableChars = getGuess' n 0 "" availableChars
 
-getGuess' :: Int -> String -> String -> IO String
-getGuess' n cword availableChars
+getGuess' :: Int -> Int -> String -> String -> IO String
+getGuess' n m cword availableChars 
     | n > 0 = do
         inp <- getChar'
         inp <- if (inp `elem` availableChars) then
-                getGuess' (n-1) (cword++(toLower inp):"") availableChars
+                getGuess' (n-1) (m+1) (cword++(toLower inp):"") availableChars
             else do
-                putChar '\b'
-                getGuess' n cword availableChars
+                if (inp == '.' && m /= 0) then do
+                    putStr "\b\b"
+                    getGuess' (n+1) (m-1) ( take (m-1) cword ) availableChars
+                else do
+                    putChar '\b'
+                    getGuess' n m cword availableChars
         return inp
 
     | otherwise = do 
