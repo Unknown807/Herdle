e5b9ff3b6fd5823d7a1d6617b104cc6d5d02a08d
mg699 <mg699@kent.ac.uk>
2022-03-01 22:51:04 +0000

    Finished iteraton 3 of part D

--- PartD.hs
+++ PartD.hs
@@ -7,20 +7,30 @@ import Data.Char
 -- iteration 1, 2 & 3
 
 go :: String -> IO()
-go word =  loop (map toLower word) ['a'..'z'] 6
+go word =  loop (map toLower word) ['a'..'z'] 1 6
 
-loop :: String -> [Char] -> Int -> IO ()
-loop _ _ 0 = putStrLn (prompt Lose) >> return ()
-loop word availableChars n = do
-    userGuess <- getGuess (length word) availableChars
-    checkedGuess <- return (checkGuess userGuess word)
-    guessStats <- return [s | (c, s) <- checkedGuess]
-    statuses <- return (showStatus guessStats)
-    putStrLn statuses
+loop :: String -> [Char] -> Int -> Int -> IO ()
+loop _ _ _ 0 = putStrLn (prompt Lose) >> return ()
+loop word availableChars attemptN loopN = do
+    putStrLn ("Attempt "++(show attemptN))
+    putStr (prompt Start)
 
-    if (and $ map (== Here) guessStats) then do
-        putStrLn (prompt Win)
+    userChoice <- getChar'
+    if (userChoice == 'q') then do
+        putStrLn ('\n' : prompt Quit)
         return ()
-    else
-        loop word (updateAvailable availableChars checkedGuess) (n-1)
+    else do
+        putStr ('\n' : prompt Guess)
+        userGuess <- getGuess (length word) availableChars
+        checkedGuess <- return (checkGuess userGuess word)
+        guessStats <- return [s | (c, s) <- checkedGuess]
+        statuses <- return (showStatus guessStats)
+        
+        putStrLn (leftMargin++statuses)
+
+        if (and $ map (== Here) guessStats) then do
+            putStrLn (prompt Win)
+            return ()
+        else
+            loop word (updateAvailable availableChars checkedGuess) (attemptN+1) (loopN-1)
 
